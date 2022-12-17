8dea694e66873cabec2180d4d500a1dd712e2ac0
Jin Li <limosmiley@gmail.com>
2022-12-17 11:33:46 +1100

    Update button language

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,12 +2,12 @@ import classnames from 'classnames'
 import './index.less'
 
 /**
- * @param {onClick} func 对外暴露的点击事件
- * @param {className} string 自定义类名
- * @param {type} string 按钮类型 primary | warning | info | default | pure
- * @param {shape} string 按钮形状 circle | radius(默认)
+ * @param {onClick} func Clicks of exposed to the outside world
+ * @param {className} string Customized class name
+ * @param {type} string Button type: primary | warning | info | default | pure
+ * @param {shape} string Button shape: circle | radius(默认)
  */
-// 按钮组件
+// Button component
 export default function Button(props) {
   let { children, onClick, className, type, shape, block } = props
   return <div className={classnames('xButton', 'ripple', type, shape, block ? 'block' : '', className)} onClick={onClick}>
